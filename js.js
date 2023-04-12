cdeb48cad7ab1c319ec71fdfe02b11aaebaddcd4
LynnArsa <lintang13arsa@gmail.com>
2023-04-13 00:16:24 +0700

    Fix Grid

--- js.js
+++ js.js
@@ -1,9 +1,25 @@
+
+
 fetch("https://api.adviceslip.com/advice")
   .then(response => response.json())
   .then(data => {
     console.log(data)
-    const container = document.querySelector('#container');
+    const container = document.querySelector('#advice-id');
+    const element = document.createElement('div');
+    element.textContent = data.slip.id;
+    container.appendChild(element);
+
+    });
+    
+fetch("https://api.adviceslip.com/advice")
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+    const container = document.querySelector('#advice-word');
     const element = document.createElement('div');
     element.textContent = data.slip.advice;
     container.appendChild(element);
+
   });
+
+  
