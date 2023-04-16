cb318c8095e936ba291aac8978c0606edb5af385
LynnArsa <lintang13arsa@gmail.com>
2023-04-17 04:01:23 +0700

    Fix JS

--- js.js
+++ js.js
@@ -1,11 +1,3 @@
-fetch("https://api.adviceslip.com/advice")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-      document.getElementById("advice-id").innerHTML = "A D V I C E  # " + data.slip.id;
-      document.getElementById("advice-word").innerHTML = data.slip.advice;
-    })
-
 function getAdvice() {
   fetch("https://api.adviceslip.com/advice")
     .then((response) => response.json())
@@ -16,4 +8,5 @@ function getAdvice() {
     })
 }
 
+getAdvice()
   
